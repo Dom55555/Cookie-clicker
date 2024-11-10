ec8cd05912400e28d297336774f2024e478aa2cd
Dom55555 <kezundominik@gmail.com>
2024-11-10 14:37:41 +0200

    load save text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,19 +3,55 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using System;
+using Unity.VisualScripting.ReorderableList;
+using System.Diagnostics;
 public class GameManager : MonoBehaviour
 {
     public TMP_Text basicAmountText;
     public TMP_Text basicPriceText;
     public TMP_Text stonesText;
     AudioSource buySound;
+
     int basicMiners = 0;
+    int goldenMiners = 0;
     int stones = 0;
+
+    float clickTimer;
+    float saveTimer;
 
     void Start()
     {
         buySound = GetComponent<AudioSource>();
+        Load();
     }
+
+    private void Update()
+    {
+        clickTimer += Time.deltaTime;
+        saveTimer += Time.deltaTime;
+        if (clickTimer >= 1)
+        {
+            for (int i = 0; i < basicMiners; i++)
+            {
+                addStones();
+            }
+            clickTimer = 0;
+        }
+        if (saveTimer >= 10)
+        {
+            Save();
+            saveTimer = 0;
+        }
+
+        stonesText.text = stones.ToString();
+        basicAmountText.text = basicMiners.ToString();
+        basicPriceText.text = "Price: " + (Math.Ceiling(50*(Math.Pow(1.3,basicMiners)))).ToString();
+    }
+    private void OnApplicationQuit()
+    {
+        Save();
+    }
+
     public void BuyBasicMiner()
     {
         int price = int.Parse(basicPriceText.text.Substring(7));
@@ -24,15 +60,25 @@ public class GameManager : MonoBehaviour
             buySound.Play();
             basicMiners++;
             stones -= price;
-            basicAmountText.text = basicMiners.ToString();
-            stonesText.text = stones.ToString();
             basicPriceText.text = "Price: " + (Math.Ceiling(price * 1.3)).ToString();
-
         }
     }
     public void addStones()
     {
         stones++;
-        stonesText.text = stones.ToString();
     }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("stones", stones);
+        PlayerPrefs.SetInt("basicMiners", basicMiners);
+        PlayerPrefs.SetInt("goldenMiners",goldenMiners);
+    }
+    void Load()
+    {
+        stones = PlayerPrefs.GetInt("stones");
+        basicMiners = PlayerPrefs.GetInt("basicMiners");
+        goldenMiners = PlayerPrefs.GetInt("goldenMiners");
+    }
+
 }
